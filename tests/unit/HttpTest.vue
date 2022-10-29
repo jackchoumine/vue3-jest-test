7e7f639dd27d8d2bb5ad36635308efd15d3d479e
jack <jackzhoumine@gmail.com>
2022-10-29 22:07:14 +0800

    使用mocks模拟模块和函数调用

---
+++ tests/unit/HttpTest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { ref, onMounted, defineComponent } from 'vue'
+import axios from 'axios'
+export default defineComponent({
+  name: 'HttpTest',
+  setup(props, { emit, attrs, slots }) {
+    onMounted(() => {
+      console.log('onMounted')
+      axios.get('/')
+    })
+    return {}
+  },
+})
+</script>
