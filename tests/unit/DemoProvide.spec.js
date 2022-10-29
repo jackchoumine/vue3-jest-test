a97e01b5f6c461240eeb95a00605e7a458c6e2ab
jack <jackzhoumine@gmail.com>
2022-10-29 20:46:41 +0800

    通过provide提供store

---
+++ tests/unit/DemoProvide.spec.js
@@ -0,0 +1,44 @@
+/*
+ * @Description : 测试 provide 和 inject
+ * @Date        : 2022-10-29 20:42:35 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2022-10-29 20:46:01 +0800
+ * @LastEditors : JackChou
+ */
+import { mount } from '@vue/test-utils'
+import Counter from './Counter.vue'
+import { createStore } from 'vuex'
+
+function createLocalStore() {
+  const store = createStore({
+    state() {
+      return { count: 0 }
+    },
+    mutations: {
+      add(state) {
+        state.count += 1
+      },
+    },
+  })
+  return store
+}
+function factory() {
+  const store = createLocalStore()
+  return mount(Counter, {
+    global: {
+      provide: {
+        store,
+      },
+    },
+  })
+}
+
+describe('DemoProvide', () => {
+  it('测试inject', async () => {
+    const wrapper = factory()
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    await button.trigger('click')
+    expect(wrapper.find('button').text()).toBe('2')
+  })
+})
---
+++ tests/unit/DemoProvide.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description : 测试 provider 和 inject 
+ * @Date        : 2022-10-29 20:38:56 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2022-10-29 20:40:56 +0800
+ * @LastEditors : JackChou
+-->
+<template>
+  <button @click="add">{{ count }}</button>
+</template>
+
+<script>
+import { computed, defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  name: 'DemoProvide',
+  setup(props, { emit, attrs, slots }) {
+    const store = inject('store')
+    const count = computed(() => {
+      return store.state.count
+    })
+    function add() {
+      store.commit('add')
+    }
+    return { count, add }
+  },
+})
+</script>
+<style scoped lang="scss"></style>
