37f02f34e00182c4d35b90d0f3d24e8beff64f0b
jack <jackzhoumine@gmail.com>
2022-10-29 23:23:20 +0800

    测试条件渲染

---
+++ tests/unit/Condition.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount } from '@vue/test-utils'
+import Condition from './Condition.vue'
+
+// NOTE 测试条件渲染
+// v-if 使用 exists
+// v-show 使用 isVisible
+describe('Condition.vue', () => {
+  it('admin 存在', () => {
+    const wrapper = shallowMount(Condition)
+    expect(wrapper.find('#admin').exists()).toBe(true)
+  })
+
+  it('admin 不存在', () => {
+    const wrapper = shallowMount(Condition, {
+      data() {
+        return {
+          admin: false,
+        }
+      },
+    })
+    expect(wrapper.find('#admin').exists()).toBe(false)
+  })
+
+  it('dev 不可见', () => {
+    const wrapper = shallowMount(Condition)
+    expect(wrapper.find('#dev').isVisible()).toBe(false)
+  })
+  it('dev 可见', () => {
+    const wrapper = shallowMount(Condition, {
+      data() {
+        return { dev: true }
+      },
+    })
+    expect(wrapper.find('#dev').exists()).toBe(true)
+    expect(wrapper.find('#dev').isVisible()).toBe(true)
+  })
+})
---
+++ tests/unit/Condition.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button">all</button>
+  <button v-if="admin" type="button" id="admin">admin</button>
+  <button v-show="dev" type="button" id="dev">dev</button>
+</template>
+<script>
+export default {
+  name: 'Condition',
+  data() {
+    return { admin: true, dev: false }
+  },
+}
+</script>
