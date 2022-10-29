9acb54432e4b35e7812310c6a6b1c4a48c1e23a3
jack <jackzhoumine@gmail.com>
2022-10-29 19:24:28 +0800

    提取工厂函数复用模板

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2022-10-29 19:03:34 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-10-29 19:15:03 +0800
+ * @LastEditTime: 2022-10-29 19:23:22 +0800
  * @LastEditors : JackChou
  */
 import { mount } from '@vue/test-utils'
@@ -12,17 +12,34 @@ const App = {
   },
   /*html*/
   template: `
-  <div>count:{{count}}</div>
+  <div v-if="count % 2 === 0">count:{{count}}. count is even.</div>
+  <div v-else>count:{{count}}. count is odd.</div>
   `,
 }
 
-test('mount', () => {
-  const wrapper = mount(App, {
-    props: {
-      count: 2,
-    },
+function factory(props) {
+  return mount(App, { props })
+}
+describe('App', () => {
+  it('should render with odd', () => {
+    const odd = 3
+    const wrapper = factory({ count: odd })
+    expect(wrapper.html()).toContain(`count:${odd}. count is odd`)
   })
-  // console.log(wrapper.html())
-  // expect(wrapper.html()).toBe('<div>hello</div>')
-  expect(wrapper.html()).toContain('count:2')
+  it('should render with even', () => {
+    const evenNumber = 4
+    const wrapper = factory({ count: evenNumber })
+    expect(wrapper.html()).toContain(`count:${evenNumber}. count is even`)
+  })
 })
+
+// test('mount', () => {
+//   const wrapper = mount(App, {
+//     props: {
+//       count: 2,
+//     },
+//   })
+//   // console.log(wrapper.html())
+//   // expect(wrapper.html()).toBe('<div>hello</div>')
+//   expect(wrapper.html()).toContain('count:2. count is even')
+// })
