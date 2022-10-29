279a0bbcef651d13ec9c8b80b88586c8a1d127a6
jack <jackzhoumine@gmail.com>
2022-10-29 20:59:25 +0800

    使用reactive提供store

---
+++ tests/unit/DemoProvide2.spec.js
@@ -0,0 +1,39 @@
+/*
+ * @Description : 使用 reactive 提供 store
+ * @Date        : 2022-10-29 20:42:35 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2022-10-29 20:58:35 +0800
+ * @LastEditors : JackChou
+ */
+import { mount } from '@vue/test-utils'
+import Counter from './Counter.vue'
+import { reactive } from 'vue'
+
+function factory() {
+  // NOTE 只有个组件使用了 provide 和 inject
+  // 不想在全局注册，app.provide('store',store)
+  // 可使用 reactive 模拟 store
+  const state = reactive({ count: 0 })
+  return mount(Counter, {
+    global: {
+      provide: {
+        store: {
+          state,
+          commit: () => {
+            state.count += 1
+          },
+        },
+      },
+    },
+  })
+}
+
+describe('DemoProvide', () => {
+  it('测试inject', async () => {
+    const wrapper = factory()
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    await button.trigger('click')
+    expect(wrapper.find('button').text()).toBe('2')
+  })
+})
