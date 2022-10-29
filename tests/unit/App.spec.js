083603451fc4e9e54dbe2965b35a7373454e728b
jack <jackzhoumine@gmail.com>
2022-10-29 19:47:07 +0800

    传递data

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2022-10-29 19:03:34 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-10-29 19:23:22 +0800
+ * @LastEditTime: 2022-10-29 19:44:19 +0800
  * @LastEditors : JackChou
  */
 import { mount } from '@vue/test-utils'
@@ -10,25 +10,44 @@ const App = {
   props: {
     count: Number,
   },
+  data() {
+    return { msg: 'hello' }
+  },
   /*html*/
   template: `
   <div v-if="count % 2 === 0">count:{{count}}. count is even.</div>
   <div v-else>count:{{count}}. count is odd.</div>
+  <h2>{{msg}}</h2>
   `,
 }
 
-function factory(props) {
-  return mount(App, { props })
+// NOTE 提取工厂函数
+function factory({ props, data }) {
+  // const app =  createApp() app.mount()
+  return mount(App, {
+    props,
+    data,
+  })
 }
 describe('App', () => {
   it('should render with odd', () => {
     const odd = 3
-    const wrapper = factory({ count: odd })
+    const hello = 'hello'
+    const wrapper = factory({
+      props: { count: odd },
+      data() {
+        return { msg: hello }
+      },
+    })
+    // NOTE 输出一个对象
+    // console.log(wrapper.vm)
     expect(wrapper.html()).toContain(`count:${odd}. count is odd`)
+    expect(wrapper.find('h2').text()).toBe(hello)
   })
+  // NOTE xit 跳过这个测试
   it('should render with even', () => {
     const evenNumber = 4
-    const wrapper = factory({ count: evenNumber })
+    const wrapper = factory({ props: { count: evenNumber } })
     expect(wrapper.html()).toContain(`count:${evenNumber}. count is even`)
   })
 })
