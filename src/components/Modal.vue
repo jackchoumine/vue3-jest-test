bb8797b44bfb4d4aed0ff59e83e445d9b8de77bd
jack <jackzhoumine@gmail.com>
2022-11-01 01:34:48 +0800

    编写弹窗

---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal-container">
+    我是弹窗组件
+    <button type="button" @click="close">点击我关闭</button>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, reactive, watch, computed, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Modal',
+  components: {},
+  setup(props, { emit, attrs, slots }) {
+    onMounted(() => {
+      console.log('onMounted')
+    })
+    function close() {
+      const modal = document.querySelector('.modal')
+      document.body.removeChild(modal)
+    }
+    return { close }
+  },
+})
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 200px;
+  z-index: 1000;
+  background-color: #ccc;
+}
+.modal-container {
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
